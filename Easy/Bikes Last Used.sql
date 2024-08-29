1f69c0f4ea808324fd0239e278db57732b57dc0b
MattParsons-MP <162918884+MattParsons-MP@users.noreply.github.com>
2024-08-29 11:28:40 +0100

    Update Bikes Last Used.sql

--- Easy/Bikes Last Used.sql
+++ Easy/Bikes Last Used.sql
@@ -5,17 +5,17 @@ Find the last time each bike was in use.
 Output both the bike number and the date-timestamp of the bike's last use (i.e., the date-time the bike was returned). 
 Order the results by bikes that were most recently used.
 
-duration:  varchar
+duration:          varchar
 duration_seconds:  int
-start_time:  datetime
-start_station:  varchar
-start_terminal:  int
-end_time:  datetime
-end_station:  varchar
-end_terminal:  int
-bike_number:  varchar
-rider_type:  varchar
-id:  int
+start_time:        datetime
+start_station:     varchar
+start_terminal:    int
+end_time:          datetime
+end_station:       varchar
+end_terminal:      int
+bike_number:       varchar
+rider_type:        varchar
+id:                int
 
 */
 
