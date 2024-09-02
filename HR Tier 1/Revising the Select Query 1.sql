4ef63da2a747f0bd89a2e620fa8b88642cd01ac4
MattParsons-MP <162918884+MattParsons-MP@users.noreply.github.com>
2024-09-02 19:27:41 +0100

    Update Revising the Select Query 1.sql

--- HR Tier 1/Revising the Select Query 1.sql
+++ HR Tier 1/Revising the Select Query 1.sql
@@ -3,7 +3,7 @@
     The CITY table is described as follows:
 
     id:          number
-    dame:        varchar2(17)
+    name:        varchar2(17)
     countrycode: varchar2(3)
     district:    varchar2(20)
     population:  number
