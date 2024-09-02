e0cee3a3ec924b212497cbadd6d895d576fe2c5c
MattParsons-MP <162918884+MattParsons-MP@users.noreply.github.com>
2024-09-02 19:20:28 +0100

    Create  Revising the Select Query 1.sql

---
+++ HR Tier 1/Revising the Select Query 1.sql
@@ -0,0 +1,17 @@
+/*  Query all columns for all American cities in the CITY table with populations larger than 100000. 
+    The CountryCode for America is USA.
+    The CITY table is described as follows:
+
+    id:          number
+    dame:        varchar2(17)
+    countrycode: varchar2(3)
+    district:    varchar2(20)
+    population:  number
+  
+*/
+
+SELECT *
+ FROM city
+  WHERE population > 100000
+    AND countrycode = 'USA'
+ ;
