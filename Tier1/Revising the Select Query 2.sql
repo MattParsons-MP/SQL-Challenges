bc1c95e296bbe130edaee7eca55c5d2eb9800d10
MattParsons-MP <162918884+MattParsons-MP@users.noreply.github.com>
2024-09-02 19:24:16 +0100

    Create Revising the Select Query 2.sql

---
+++ Tier1/Revising the Select Query 2.sql
@@ -0,0 +1,19 @@
+/* 
+
+Query the NAME field for all American cities in the CITY table with populations larger than 120000. 
+
+The CountryCode for America is USA.
+
+The CITY table is described as follows:
+
+    id:          number
+    dame:        varchar2(17)
+    countrycode: varchar2(3)
+    district:    varchar2(20)
+    population:  number
+
+*/
+SELECT name
+    FROM city
+    WHERE population > 120000
+        AND countrycode = 'USA';
