b4e69e6a54b729895d772a09884e4069b8068f1e
MattParsons-MP <162918884+MattParsons-MP@users.noreply.github.com>
2024-09-02 19:27:16 +0100

    Create Select All.sql

---
+++ HR Tier 1/Select All.sql
@@ -0,0 +1,14 @@
+/*  Query all columns (attributes) for every row in the CITY table.
+
+The CITY table is described as follows:
+
+    id:          number
+    name:        varchar2(17)
+    countrycode: varchar2(3)
+    district:    varchar2(20)
+    population:  number
+
+*/
+
+SELECT * 
+    FROM city;
