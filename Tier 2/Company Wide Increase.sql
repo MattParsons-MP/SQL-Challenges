99456f3b386080e9cfa36286e18e79e031db7579
MattParsons-MP <162918884+MattParsons-MP@users.noreply.github.com>
2024-09-16 19:42:18 +0100

    Create Company Wide Increase.sql

---
+++ Tier 2/Company Wide Increase.sql
@@ -0,0 +1,24 @@
+/*
+
+If our company hits its yearly targets, every employee receives a salary increase depending on what level you are in the company.
+
+Give each Employee who is a level 1 a 10% increase, level 2 a 15% increase, and level 3 a 200% increase.
+
+Include this new column in your output as "new_salary" along with your other columns.
+
+Table:
+
+employee_id:  	int
+pay_level:    	int
+salary:        	int
+
+*/
+
+SELECT *,
+  CASE
+      WHEN pay_level = 1 THEN salary * 1.1
+      WHEN pay_level = 2 THEN salary * 1.15
+      WHEN pay_level = 3 THEN salary * 3
+  END AS new_salary
+FROM employees 
+;
